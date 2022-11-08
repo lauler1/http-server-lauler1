d46b7f3725e0a366358a17fe7a5715f21ca147ed
lauler1 <93792331+lauler1@users.noreply.github.com>
2022-11-08 14:35:04 +0100

    1 file modified, 3 added

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-var http = require('http');
+var fs = require('fs'),
+    http = require('http');
 
-http
-  .createServer(function (req, res) {
-    res.write('Hello, Node.js!'); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+http.createServer(function (req, res) {
+  fs.readFile(__dirname + req.url, function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}).listen(8080); //the server object listens on port 8080
 
 console.log('Server running on port 8080');
