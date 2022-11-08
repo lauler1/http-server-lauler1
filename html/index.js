b0b0ee0a83da83743d9943d7c8a2e6ba432b9ed0
lauler1 <93792331+lauler1@users.noreply.github.com>
2022-11-08 17:21:30 +0100

    5 files modified

--- html/index.js
+++ html/index.js
@@ -1,6 +1,31 @@
+import { fluentDataGrid, provideFluentDesignSystem } from "@fluentui/web-components";
+//fluentDataGrid = require("@fluentui/web-components");
+//provideFluentDesignSystem  =require ("@fluentui/web-components");
 
-	function myFunction() {
-	  document.getElementById("demo").innerHTML = "Paragraph changed.";
+provideFluentDesignSystem().register(fluentDataGrid);
+
+// populate the data-grid
+let defaultGridElement  = null;
+defaultGridElement = document.getElementById('defaultGrid');
+
+defaultGridElement.rowsData = newDataSet(10);
+
+function newDataSet(rowCount) {
+	const newRows = [];
+	for (let i = 0; i <= rowCount; i++) {
+		newRows.push(newDataRow(`${i + 1}`));
+	}
+	return newRows;
+	};
+
+function newDataRow(id) {
+	return {
+		rowId: `rowid-${id}`,
+		item1: `value 1-${id}`,
+		item2: `value 2-${id}`,
+		item3: `value 3-${id}`,
+		item4: `value 4-${id}`,
+	};
 	};
 
 
--- server.js
+++ server.js
@@ -2,12 +2,31 @@ var fs = require('fs'),
     http = require('http');
 
 http.createServer(function (req, res) {
+
+  console.log("req.url="+req.url);
   fs.readFile(__dirname + req.url, function (err,data) {
     if (err) {
+      console.log("err="+err);
       res.writeHead(404);
       res.end(JSON.stringify(err));
       return;
     }
+
+    if(req.url.includes(".js")){
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      console.log('text/javascript');
+    }else if (req.url.includes(".css")){
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      console.log('text/css');
+    }else if (req.url.includes(".html")){
+      res.writeHead(200, {'Content-Type': 'application/xhtml'});
+      console.log('application/xhtml+xml');
+    }else{
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      console.log('text/plain');
+    }
+
+
     res.writeHead(200);
     res.end(data);
   });
