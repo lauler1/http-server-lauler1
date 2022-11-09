ba2ea05d3c1c2b4051c7e14ebe9830481841dc55
lauler1 <auler.leandro@gmail.com>
2022-11-09 10:20:54 +0100

    Module script moved from index.html to index.js

--- html/index.js
+++ html/index.js
@@ -1,3 +1,17 @@
 
+    import { fluentDataGrid, provideFluentDesignSystem } from "https://unpkg.com/@fluentui/web-components@2.0.0";
+    import { fluentCheckbox, fluentButton} from "https://unpkg.com/@fluentui/web-components@2.0.0";
 
+    provideFluentDesignSystem().register(fluentCheckbox());
+    provideFluentDesignSystem().register(fluentDataGrid);
+    provideFluentDesignSystem().register(fluentButton());
 
+    // populate the data-grid
+    let defaultGridElement  = null;
+    defaultGridElement = document.getElementById('defaultGrid');
+
+    defaultGridElement.rowsData = [
+        { item1: "value 1-1", item2: "value 2-1" },
+        { item1: "value 1-2", item2: "value 2-2" },
+        { item1: "value 1-3", item2: "value 2-3" },
+    ];
